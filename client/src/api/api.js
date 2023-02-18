b402a4cc68487a7f0fd4f4a0af9b7d1b32906edc
cornelius mutuku <80700768+gradan-hash@users.noreply.github.com>
2023-02-18 22:11:28 +0300

    Delete client directory

--- client/src/api/api.js
+++
@@ -1,8 +0,0 @@
-const host = "http://localhost:3003";
-
-const loginRoute = `${host}/api/auth/login`;
-const registerRoute = `${host}/api/auth/register`;
-const BeehiveRoute = `${host}/api/hives`;
-
-
-export { host, BeehiveRoute,loginRoute, registerRoute };
